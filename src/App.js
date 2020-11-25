da7f7567be209587c72e29da0fc9e05af27180d9
josiahprathaban <josiah.prathaban@gmail.com>
2020-11-25 17:02:30 +0530

    Create Navigation Bar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Welcome from "./components/Welcome";
 import './App.css';
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
+        <NavBar/>
         <Welcome/>
     </div>
   );
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -3,10 +3,46 @@ import './Welcome.css';
 import logo from "../logo.svg";
 
 class Welcome extends Component{
+    i=0;
+    constructor(props) {
+        super(props);
+        this.state = {keyWord : "believable"}
+    }
+
+    componentDidMount() {
+        this.word = setInterval(
+            ()=> this.wordChange(),
+            500
+        );
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.word);
+    }
+
+    wordChange(){
+        if(this.i==0) {
+            this.setState({keyWord: "breakable"});
+            this.i = 1;
+        }else if(this.i==1) {
+            this.setState({keyWord: "thinkable"});
+            this.i = 2;
+        }else if(this.i==2) {
+            this.setState({keyWord: "believable"});
+            this.i = 0;
+        }
+    }
+
     render() {
         return(
-            <div>
-                <img className={'logo'} src={logo}/>
+            <div className={'welcome'}>
+                <div className={'logo'}>
+                    <img src={logo}/>
+                </div>
+                <div className={'moto'}>
+                    <div className={'left'}>We are UN</div>
+                    <div className={'right'}>{this.state.keyWord}</div>
+                </div>
             </div>
         );
     }
